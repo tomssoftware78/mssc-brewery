e652f1121bb4fcf4fdb1047417a0725fef899367
tomvde <D-8110tr>
2019-12-24 11:27:24 +0100

    POST request for Beer

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -17,4 +17,12 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("Baskwadder blond")
+                .build();
+    }
 }
